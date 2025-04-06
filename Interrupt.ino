4e6798d09e750c173245ac7185416baa5ef71bae
bdimsim <161640204+bdimsim@users.noreply.github.com>
2025-04-06 21:46:57 +1000

    Added comments

--- Interrupt.ino
+++ Interrupt.ino
@@ -7,39 +7,44 @@
 #include <SAMDTimerInterrupt.h>
 
 #define DHTTYPE DHT11
-#define HW_INTERVAL_MS 500L // Timer interval for the internal interrupt
-#define TIMER0_INTERVAL_MS 1000L
-#define TIMER1_INTERVAL_MS 500L
-#define TIMER2_INTERVAL_MS 2000L
+#define HW_INTERVAL_MS 500L
+#define TIMER0_INTERVAL_MS 1000L // Blue LED toggle every 1s
+#define TIMER1_INTERVAL_MS 500L // Light sensor read every 0.5s
+#define TIMER2_INTERVAL_MS 2000L // DHT read every 2s
 
 const uint8_t buttonPin = 2; // Pin for the button
-const uint8_t yellowLedPin = 17;
-const uint8_t blueLedPin = 20;
-const uint8_t redLedPin = 21;
+const uint8_t yellowLedPin = 17; // Pin for the yellow LED
+const uint8_t blueLedPin = 20; // Pin for the blue LED
+const uint8_t redLedPin = 21; // Pin for the red LED
 const uint8_t dhtPin = 14; // Pin for the DHT sensor
 
+// Volatile states of LEDs since they are modified in the ISR
 volatile uint8_t blueLedState = LOW;
 volatile uint8_t redLedState = LOW;
 volatile uint8_t yellowLedState = LOW;
 
-BH1750 lightSensor;
-DHT dht(dhtPin, DHTTYPE);
+// Sensor objects
+BH1750 lightSensor; // Light sensor
+DHT dht(dhtPin, DHTTYPE); // DHT11
 
-SAMD_ISR_Timer ISR_Timer;
-SAMDTimer ITimer(TIMER_TC3);
+SAMD_ISR_Timer ISR_Timer; // ISR timer (1 hardware timer can service up to 16 ISR timers)
+SAMDTimer ITimer(TIMER_TC3); // Use the hardward TC3 timer for the main timer
 
+// Main timer handler called every 500ms
 void TimerHandler() {
-  // This function is called every 500ms
-  ISR_Timer.run(); // Call the timer interrupt handler
+  // checks if any of the software timers have reached their scheduled 
+  // interval and calls the associated callback function such as toggleBlueLed() 
+  // that is scheduled to be called every 1000ms (and checked every 500ms)
+  ISR_Timer.run();
 }
 
 void buttonRedLedToggle() {
-  static unsigned long lastInterruptTime = 0;
+  static unsigned long lastInterruptTime = 0; // Store the last interrupt time
 
-  if (millis() - lastInterruptTime > 200) {
+  if (millis() - lastInterruptTime > 200) { // Mitigate button bouncing (rapid button activations due to vibration)
     redLedState = !redLedState; // Toggle the LED state
     digitalWrite(redLedPin, redLedState); // Update the LED
-    lastInterruptTime = millis();
+    lastInterruptTime = millis(); // Update the last interrupt time
   }
 }
 
@@ -49,20 +54,21 @@ void toggleBlueLed() {
 }
 
 void updateLightSensorAndYellowLed() {
-  float lux = lightSensor.readLightLevel();
+  float lux = lightSensor.readLightLevel(); // Read light level
   yellowLedState = lux > 400 ? HIGH : LOW; // Set yellow LED state based on light level
-  digitalWrite(yellowLedPin, yellowLedState);
+  digitalWrite(yellowLedPin, yellowLedState); // Update the LED
 }
 
 void readAndPrintDhtData() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float h = dht.readHumidity(); // Read humidity
+  float t = dht.readTemperature(); // Read temperature
 
-  if (isnan(h) || isnan(t)) {
+  if (isnan(h) || isnan(t)) { // Check if the readings are valid
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
+  // Print the readings (message) to the serial monitor
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%, Temperature: "));
@@ -71,23 +77,28 @@ void readAndPrintDhtData() {
 }
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP); // HIGH
-  pinMode(dhtPin, INPUT_PULLUP); // HIGH
-  pinMode(yellowLedPin, OUTPUT); // LOW
-  pinMode(blueLedPin, OUTPUT); // LOW
-  pinMode(redLedPin, OUTPUT); // LOW
+  // Set pin modes as input and output
+  pinMode(buttonPin, INPUT_PULLUP); // default: HIGH
+  pinMode(dhtPin, INPUT_PULLUP); // default: HIGH
+  pinMode(yellowLedPin, OUTPUT); // default: LOW
+  pinMode(blueLedPin, OUTPUT); // default: LOW
+  pinMode(redLedPin, OUTPUT); // default: OW
 
-  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonRedLedToggle, FALLING); // Toggle red LED on button press (external interrupt)
-  ITimer.attachInterruptInterval_MS(HW_INTERVAL_MS, TimerHandler); // Call TimerHandler every 500ms (internal interrupt)
+  // Attach interrupts
+  // External interrupt on button pin to toggle red LED
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonRedLedToggle, FALLING);
+  // Internal interrupt on timer to call TimerHandler every 0.5s
+  ITimer.attachInterruptInterval_MS(HW_INTERVAL_MS, TimerHandler);
 
-  ISR_Timer.setInterval(TIMER0_INTERVAL_MS, toggleBlueLed); // Toggle blue LED every second
+  // Attach software timers to their respective functions
+  ISR_Timer.setInterval(TIMER0_INTERVAL_MS, toggleBlueLed); // Toggle blue LED every 1000ms
   ISR_Timer.setInterval(TIMER1_INTERVAL_MS, updateLightSensorAndYellowLed); // Read light sensor every 500ms and toggle yellow LED
-  ISR_Timer.setInterval(TIMER2_INTERVAL_MS, readAndPrintDhtData); // Read DHT sensor every 2 seconds and print to serial
+  ISR_Timer.setInterval(TIMER2_INTERVAL_MS, readAndPrintDhtData); // Read DHT sensor every 2 seconds and print a message to serial
 
   Serial.begin(115200);
   Wire.begin(); // Enables I2C communication (SDA and SCL pins)
   lightSensor.begin(); // Begin measurement with default I2C address 0x23
-  dht.begin();
+  dht.begin(); // Begin DHT sensor
 }
 
 void loop() {
